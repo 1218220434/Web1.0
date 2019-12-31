53dfbc2e7d3c42f119afa5b31d75481100c1e3e9
hcl <1218220434@qq.com>
2019-12-31 18:07:18 +0800

    何昌龙第四版

--- src/js/index.js
+++ src/js/index.js
@@ -15,25 +15,21 @@ import echarts from 'echarts';
 
 // 轮播图
 let mySwiper = new Swiper('.swiper-container', {
-        loop: true, // 循环模式选项
-        // 自动播放
-        autoplay: true,
-        // 切换效果
-        effect: 'coverflow',
-        // 如果需要分页器
-        pagination: {
-            el: '.swiper-pagination',
-        },
-        // 如果需要前进后退按钮
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-        }
-    })
-    // $(window).bind("load resize",function(){
-    //     2     document.documentElement.clientWidth >= 600 ? console.log("hello") : console.log("xiaoyu600")
-    //     3     });
-    //     4     //第二种
-    //     5     $(window).bind("load resize",function(){
-    //     6      if(document.documentElement.clientWidth >= 600) console.log("hello111")
-    //     7     });
+    loop: true, // 循环模式选项
+    // 自动播放
+    autoplay: true,
+    // 切换效果
+    effect: 'coverflow',
+    // 如果需要分页器
+    pagination: {
+        el: '.swiper-pagination',
+    },
+    // 如果需要前进后退按钮
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    }
+})
+$(window).bind("load resize", function() {
+    $(".search").css("top", document.documentElement.clientWidth / 2.95)
+});
