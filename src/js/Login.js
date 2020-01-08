8b1b65699da381d21fc73bcdc648c0c0c18c9968
hcl <1218220434@qq.com>
2020-01-09 01:07:23 +0800

    评论页面

--- src/js/Login.js
+++ src/js/Login.js
@@ -28,9 +28,10 @@ import '../public/footer.js';
 
 $(window).bind("load resize", function() {
     let screenDom = document.documentElement.clientWidth;
-    $(".main-bg").css("top", screenDom / 80)
-        .css("right", screenDom / 30)
-
+    $(".loSMA").css({
+        "top": 150 - (1500 - screenDom) / 5,
+        "right": 250 - (1500 - screenDom) / 3.125
+    })
 });
 
 const BASE_URL = "http://192.168.7.5:8000/zjcx";
@@ -39,44 +40,43 @@ const BASE_URL = "http://192.168.7.5:8000/zjcx";
 // 这里处理第一次跨域存储用户信息
 
 $(".denglu").on("click", function() {
-    console.log($("#usero").val(), $("#pass").val())
-    $.ajax({
-        url: `${BASE_URL}/login/`,
-        type: "POST",
-        dataType: "json",
-                     // contentType:'application/json',
-        data: {
-            username: $("#usero").val(),
-            password: $("#pass").val()
-        },
-    }).done(res => {
-        console.log('登陆成功，即将跳转首页')
-        window.location.href = "/index.html";
-        return res.json();
-    }).fail(err => {
-        if (err.status == 401) {
-            console.log("账号或者密码错误")
-        } else { console.log(err.responseText) }
-    });
+    console.log("不是第一次登录登录，三秒后进入主页")
+    localStorage.setItem("user_img", "头像")
+    setTimeout(function() {
+            window.location.href = "/index.html";
+        }, 2000)
+        // console.log($("#usero").val(), $("#pass").val())
+        // $.ajax({
+        //     url: `${BASE_URL}/login/`,
+        //     type: "POST",
+        //     dataType: "json",
+        //                  // contentType:'application/json',
+        //     data: {
+        //         username: $("#usernamelo").val(),
+        //         password: $("#passwordlo").val()
+        //     },
+        // }).done(res => {
+        //     console.log('登陆成功，即将跳转首页')
+        //     if (res.code = 0) {
+        //         console.log("第一次登录,进入爱好选择")
+        //         window.location.href = "../pages/myhobby-page.html";
+        //         //存储头像备用
+        //         localStorage.setItem("user_img", "头像")
+        //     } else {
+        //         console.log("不是第一次登录登录，三秒后进入主页")
+        //         localStorage.setItem("user_img", "头像")
+        //         setTimeout(function() {
+        //             window.location.href = "/index.html";
+        //         }, 2000)
 
-    // fetch(`${BASE_URL}/login`, {
-    //     method: "POST",
-    //     headers: {
-    //         "Content-Type": "application/json"
-    //     },
-    //     body: JSON.stringify({
-    //         loguser: loguser.value,
-    //         logpawo: logpawo.value,
-    //     })
-    // }).then(res => {
-    //     return res.json();
-    // }).then(data => {
-    //     console.log(data);
-    //     if (data.code == 200) {
-    //     } else if (data.data == "用户不存在") {
-    //         console.log("用户不存在")
-    //     } else {
-    //         console.log("服务繁忙，请稍后")
     //     }
+
+    //     return res.json();
+    // }).fail(err => {
+    //     if (err.status == 401) {
+    //         console.log("账号或者密码错误")
+    //     } else { console.log(err.responseJSON) }
     // });
+
+
 })
