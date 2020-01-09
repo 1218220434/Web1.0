50de3a5b9bb5377beeeea0cc455169e0f9bc6d98
Duandan <626264312@qq.com>
2020-01-09 09:03:13 +0800

    duan9

--- src/js/share-page.js
+++ src/js/share-page.js
@@ -7,11 +7,11 @@ import '../public/footer.less';
 import '../public/footer.js';
 
 
-$(document).ready(function() {
+$(document).ready(function () {
 
 
     //随浏览器变化而变化
-    $(window).bind("load resize", function() {
+    $(window).bind("load resize", function () {
         let screenDom = document.documentElement.clientWidth;
         $("main p").css("fontSize", screenDom / 120)
         $("h4").css("fontSize", screenDom / 100)
@@ -21,6 +21,8 @@ $(document).ready(function() {
         $(".box").css("height", screenDom / 3.5)
         $(".bigbox").css("height", ($(".box").height() + 40) * 3.08)
         $(".content").css("height", $(".bigbox").height() + 70)
+        $("textarea").css("width", $(".details").width())
+        $("textarea").css("height", $(".details").height())
     });
     //选项卡
     let menu_items = [...document.querySelectorAll(".tab .select")];
@@ -29,9 +31,9 @@ $(document).ready(function() {
     // 2. 定义变量记录上一次选中的下标，默认值为0
     let last_sel_index = 0;
     // 3. 遍历菜单项
-    menu_items.forEach(function(menu_item) {
+    menu_items.forEach(function (menu_item) {
         // 4. 添加点击事件
-        menu_item.onclick = function() {
+        menu_item.onclick = function () {
             // 5. 获取单击的当前菜单项的下标
             let index = menu_items.indexOf(this);
             // 6. 通过移除class实现移除上一次菜单项选中和内容显示的效果
@@ -64,78 +66,19 @@ $(document).ready(function() {
     // })
     // //插入图片
 
-    // function imgPreview(fileDom) {
-    //     // 判断是否支持FileReader 
-    //     let reader = null;
-    //     if (window.FileReader) {
-    //         reader = new FileReader();
-    //     } else {
-    //         alert("您的设备不支持图片预览功能，如需该功能请升级您的设备！");
-    //         return;
-    //     }
-    //     // 获取选中的文件
-    //     let file = fileDom.files[0];
-    //     // 判断是否是图片类型
-    //     let imageType = /^image\//;
-    //     if (!imageType.test(file.type)) {
-    //         alert("请选择图片！");
-    //         return;
-    //     }
-    //     // 读取完成
-    //     reader.onload = function (e) {
-    //         // 图片路径设置为读取的图片
-    //         // img.src = e.target.result;
-    //         let box = document.querySelector(".case");
-    //         // 回显图片
-    //         box.style.backgroundImage = `url(${e.target.result})`;
-    //     }
-    //     // 读取图片 => 将图片转换成base64
-    //     reader.readAsDataURL(file);
-    // }
 
-    // function uploadImage() {
-    //     let file = document.querySelector(".case input").files[0];
-    //     if (!file) {
-    //         alert("点击上方方框选择图片！");
-    //         return;
-    //     }
-    //     let formData = new FormData();
-    //     formData.append("photo", file);
-    //     // ajax
-    //     let xhr = new XMLHttpRequest();
-    //     xhr.open("POST", "http://localhost:8800/up_image", true);
-    //     xhr.send(formData);
-    //     xhr.onload = function (res) {
-    //         console.log(res);
-    //     }
-    // }
-    //
-    $(".tabControl .tab .select").click(function(){
+    $(".tabControl .tab .select").click(function () {
         $(this).addClass("show-c").siblings().removeClass("show-c");
     }).first().trigger("click");
-    //ditu
-    var site = ""
-    var _af = document.querySelector(".af")
-    _af.onclick = function() {
-            document.querySelector("#allmap").classList.add("op")
-        } // 百度地图API功能
-    var map = new BMap.Map("allmap"); // 创建Map实例
-    map.centerAndZoom("成都市天府三街天府软件园", 19); // 初始化地图,用城市名设置地图中心点
-    map.enableScrollWheelZoom(); // 缩放功能
-    var geoc = new BMap.Geocoder();
-    map.addEventListener("click", function(e) {
-        var what = e.point.lng + "," + e.point.lat
-        var _point = new BMap.Point(e.point.lng, e.point.lat);
-        var marker = new BMap.Marker(_point); // 创建标注
-        map.addOverlay(marker); // 将标注添加到地图中
-        //    定位控件
-        var pt = e.point;
-        geoc.getLocation(pt, function(rs) {
-            //addressComponents对象可以获取到详细的地址信息
-            var addComp = rs.addressComponents;
-            site = addComp.province + addComp.city + addComp.district + addComp.street + addComp.streetNumber;
-            //将对应的HTML元素设置值
-        });
-    });
-    console.log(site)
+
+    //发布框
+    $(".displayer").click(function () {
+        $(this).fadeOut("slow")
+        $(".publish").slideDown("slow");
+    })
+    // $("#pub").click(function () {
+    //     $(".publish").animate({ height: "300px", width: "550px" });
+    //     $(".anniu").fadeIn("slow");
+    //     $(".photo").fadeIn("slow");
+    // });
 });
